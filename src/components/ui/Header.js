7beced4b2460f99d38898ffab467c6740a22047e
Sergio Rosa <scarlosjazz@gmail.com>
2020-07-20 12:42:55 -0400

    chore:  header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
+
+//ElevationScroll
+function ElevationScroll(props) {
+  const { children } = props;
+  
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+//Header
 export default function Header(props){
   return(
+    <ElevationScroll>
     <AppBar position="fixed">
       <ToolBar>
         Sergio Devlopment
       </ToolBar>
     </AppBar>
+    </ElevationScroll>
   );
 
 }
