fd768de8e2abefcb291e89b6f783786ec0428b2b
Sergio Rosa <scarlosjazz@gmail.com>
2020-07-20 16:33:13 -0400

    chore:  theming part II Material styles

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,3 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+//var() -> Custom Properties (Variables)
+const headBlue = "#0B72B9"
+const headOrange = "#FFBA60"
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${headBlue}`,     
+      orange: `${headOrange}`     
+    },
+    primary: {
+      main: `${headBlue}`   
+    },
+    secondary: {
+      main: `${headOrange}`   
+    
+    }
+  }
+});
