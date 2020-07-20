e8eeb9e03b8bbed2266049064b15c51698c561b9
Sergio Rosa <scarlosjazz@gmail.com>
2020-07-20 12:30:50 -0400

    chore:  header

---
+++ src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+
+export default function Header(props){
+  return(
+    <AppBar position="fixed">
+      <ToolBar>
+        Sergio Devlopment
+      </ToolBar>
+    </AppBar>
+  );
+
+}
+
+
