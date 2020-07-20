93ee7ace909e6313fef40386347ff3acc318e19c
Sergio Rosa <scarlosjazz@gmail.com>
2020-07-20 16:15:20 -0400

    chore:  theming part I

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Header from '../components/ui/Header';
+import theme from './ui/Theme';
 
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
     <Header/>
     Hello!
-    </div>
+    </ThemeProvider>
   );
 }
 
